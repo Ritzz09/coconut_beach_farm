beee15b6f9c8942680d6597e390f3d51eb812282
Ritzz09 <seexpert111@gmail.com>
2025-08-03 19:02:50 +0530

    error

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./src/components/navbar";
 import HeroSection from "./src/components/hero";
 import AmenitiesSection from "./src/components/amenities";
 import ScrollImageReveal from "./src/components/rooms";
-import Gallery from "./src/components/galllery";
+import Gallery from "./src/components/gallery";
 
 function App() {
   return (
