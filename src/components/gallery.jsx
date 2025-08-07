1c6f42d9076e97fca492b09ae93df4cca062b132
Ritzz09 <seexpert111@gmail.com>
2025-08-07 12:46:38 +0530

    gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -11,10 +11,20 @@ import img6 from "../assets/gallery/6.jpeg";
 import img7 from "../assets/gallery/7.jpeg";
 import img8 from "../assets/gallery/8.jpg";
 import img9 from "../assets/gallery/9.jpeg";
+import img10 from "../assets/gallery/10.jpeg";
+import img11 from "../assets/gallery/11.jpeg";
+import img12 from "../assets/gallery/12.jpeg";
+import img13 from "../assets/gallery/13.jpeg";
+import img14 from "../assets/gallery/14.jpeg";
+import img15 from "../assets/gallery/15.jpeg";
+import img16 from "../assets/gallery/16.jpeg";
+import img17 from "../assets/gallery/17.jpeg";
+import img18 from "../assets/gallery/18.jpeg";
 
-const row1Images = [img1, img2, img3, img1, img2, img3];
-const row2Images = [img5, img6, img7, img1, img2, img3];
-const row3Images = [img9, img4, img8, img1, img2, img3];
+
+const row1Images = [img1, img2, img3, img4, img5, img6];
+const row2Images = [img7, img8, img9, img10, img11, img12];
+const row3Images = [img13, img14, img15, img16, img17, img18];
 
 const Gallery = () => {
   const allRows = [row1Images, row2Images, row3Images];
--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import bgImage from "../assets/gallery/3.jpeg";
+import bgImage from "../assets/gallery/1.jpeg";
 
 export default function ThankYou() {
-  return (
-    <div
-      className="min-h-screen bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: `url(${bgImage})` }} // ✅ fixed path
-    >
-      <div className="bg-white/90 p-20 rounded-2xl shadow-2xl text-center max-w-2xl">
-        <h1 className="text-4xl font-bold text-yellow-500 mb-4">Thank you! We've received your request.</h1>
-        <p className="text-2xl text-gray-700 mb-6">
-        Thank you for choosing Ardent Smiles. We’ll contact you shortly to confirm the
-details and look forward to crafting your smile with passion.
-        </p>
-        <Link
-          to="/"
-          className="bg-gradient-to-r from-yellow-300 to-amber-500 text-black border-black border-2 font-semibold px-5 py-2 rounded-full hover:scale-110 hover:border-b-6 shadow-md"        >
-          Back to Home
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="min-h-screen bg-cover bg-center flex items-center justify-center z-0"
+            style={{ backgroundImage: `url(${bgImage})` }} // ✅ fixed path
+        >
+            <div className="bg-slate-950/80 p-20 rounded-2xl shadow-2xl text-center max-w-2xl z-10 border-2 border-white/30 ">
+                <h1 className="text-4xl font-bold text-sky-200 mb-4">Thank you! We've received your request.</h1>
+                <p className="text-2xl text-white mb-6">
+                    Thank you for choosing Coconut Beach Farm, your serene escape by the sea! <br/>
+                    We’ve received your request and our team will get in touch with you shortly to confirm the details.
+                </p>
+                <Link
+                    to="/"
+                     className="hero-button opacity-100  mt-8 relative inline-flex items-center justify-center px-8 py-4 overflow-hidden font-bold text-white transition duration-300 ease-out border-2 border-white rounded-full group">
+                   
+                        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-white/30 group-hover:translate-x-0 ease">
+                            <svg
+                                className="w-8 h-8 text-sky-400"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M13 7l5 5m0 0l-5 5m5-5H6"
+                                />
+                            </svg>
+                        </span>
+                        <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+                        Home
+                        </span>
+                        <span className="relative invisible">Home</span>
+                 
+                </Link>
+            </div>
+            <div className="absolute inset-0 bg-black/30 z-1"></div>
+
+        </div>
+    );
 }
